7750848a0ab191c28111208c159a78d32f7db228
1Huahuo <563592416@qq.com>
2026-03-30 22:23:32 +0800

    3.30_gzh

---
+++ 3.独立文件/0006.生产者-消费者/main.c
@@ -0,0 +1,12 @@
+#include <my_header.h>
+
+typedef struct node_s{
+    int value;
+    struct node_s* next
+}
+
+int main(int argc, char* argv[]){                                  
+    printf("Hello world\n");
+
+    return 0;
+}
